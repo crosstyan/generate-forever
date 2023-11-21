1cc2168505b12c7c28701fdd6a16bc8f66b78c49
crosstyan <crosstyan@outlook.com>
2023-11-21 09:52:47 +0800

    fix: bump version

--- src/prefix.js
+++ src/prefix.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Generate Forever
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.1.1
 // @description  create a generate forever button
 // @author       Crosstyan
 // @match        *://novelai.net/image*
